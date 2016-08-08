b77bf330c502c9c73e7fea6f5c7855429d9893c0
qintianxiang <txqin@wisedu.com>
2016-08-08 16:31:06 +0800

    提交tooltip的demo和emap form测试数据

--- src/pages/demo/tooltip/tooltip.vue
+++ src/pages/demo/tooltip/tooltip.vue
@@ -6,22 +6,55 @@
             </header>
             <div class="bh-mv-8 bh-mb-16">
                 <bh-card class='content bh-p-8'>
-                    <div class='box' v-bh-tooltip='{content: "自动关闭"}'>默认行为</div>
-                    <div class='box' v-bh-tooltip='{content: "点击我才能关闭", autoHide: false}'>点击才能关闭</div>
-                    <div class='box' v-bh-tooltip='{content: "方向向上", position: "top"}'>向上</div>
-                    <div class='box' v-bh-tooltip='{content: "方向向左", position: "left"}'>向左</div>
-                    <div class='box' v-bh-tooltip='{content: "方向向右", position: "right"}'>向右</div>
+                    <div v-el:tt1 class='box'>默认行为</div>
+                    <div class='box box1'>手动关闭</div>
+                    <div class='box box2'>向上</div>
+                    <div class='box box3'>向左</div>
+                    <div class='box box4'>向右</div>
+                    <div class='box box5'>向下</div>
                 </bh-card>
             </div>
+            <bh-tooltip :target='target'>
+                <p>
+                    这是提示内容
+                </p>
+                <a href="#">
+                    支持html格式
+                </a>
+            </bh-tooltip>
+            <bh-tooltip target='.box1' :auto-hide='false' :show-footer='true'>
+                我会自动关闭@@
+            </bh-tooltip>
+            <bh-tooltip target='.box2' position='top'>
+                方向向上
+            </bh-tooltip>
+            <bh-tooltip target='.box3' position='left'>
+                方向向左
+            </bh-tooltip>
+            <bh-tooltip target='.box4' position='right'>
+                方向向右
+            </bh-tooltip>
+            <bh-tooltip target='.box5' position='bottom'>
+                方向向下
+            </bh-tooltip>
         </section>
     </article>
 </template>
 
 <script>
     import BhCard from 'components/bh-card/bhCard';
+    import BhTooltip from 'components/bh-tooltip/bhTooltip';
 
     export default {
-        components: {BhCard}
+        data () {
+            return {
+                target: null
+            };
+        },
+        ready () {
+            this.target = this.$els.tt1;
+        },
+        components: {BhCard, BhTooltip}
     };
 </script>
 
