64106589acef17b13e0d5cf042906facb72a069e
qintianxiang <txqin@wisedu.com>
2017-08-29 11:29:30 +0800

    excel upload demo

--- src/pages/demo/excelupload/excelUpload.vue
+++ src/pages/demo/excelupload/excelUpload.vue
@@ -25,8 +25,15 @@
         data: () => ({
             options1: {
                 btnText: '导入Excel（成功）',
+                // filetype: ['xls'],
                 url: 'http://172.16.7.75:8000/bh-vue/excelimport/upload',
-                doCheck () {
+                beforeSubmit (e, data) {
+                    data.formData = {a: 1, b: 2};
+                },
+                onFileUploaded (e, data) {
+                    return data.result.code === '0';
+                },
+                doCheck (data) {
                     // 模拟检查成功响应
                     return new Promise(resolve => {
                         setTimeout(() => {
@@ -34,7 +41,7 @@
                         }, 1000);
                     });
                 },
-                doImport () {
+                doImport (e, opt, downTplData, options) {
                     // 模拟导入成功响应
                     return new Promise(resolve => {
                         setTimeout(() => {
@@ -46,13 +53,28 @@
             options2: {
                 btnText: '导入Excel（上传失败）',
                 url: 'http://172.16.7.75:8000/bh-vue/excelimport/404here',
-                doCheck: function () {}
+                doCheck: function (data) {},
+                downloadTmplFile (e, opt, downTplData, options) {
+                    console.log('downloading template file...');
+                }
             },
             options3: {
                 btnText: '导入Excel（校验失败）',
                 url: 'http://172.16.7.75:8000/bh-vue/excelimport/upload',
-                doCheck: function () {
+                doCheck: function (data) {
                     return Promise.resolve({isSuccess: false, success: 10, total: 20});
+                },
+                downloadErrorFile: function (e, opt, downTplData, options) {
+                    console.log(e);
+                },
+                doImport (e, opt, downTplData, options) {
+                    // 模拟导入成功响应
+                    console.log('只导入正确的数据...');
+                    return new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve({count: 10});
+                        }, 1000);
+                    });
                 }
             }
         }),
